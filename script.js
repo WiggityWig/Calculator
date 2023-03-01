f20afa378da0a94d0a5b75b7bd2535be5aeafb21
WiggityWig <122560716+WiggityWig@users.noreply.github.com>
2023-03-01 06:44:18 -0500

    create basic arithmetic functions

---
+++ script.js
@@ -0,0 +1,16 @@
+//addition
+const add = (a,b) => {
+    return a + b;
+}
+//subtraction
+const sub = (a,b) => {
+    return a - b;
+}
+//multiplication
+const mult = (a,b) => {
+    return a * b;
+}
+//division
+const divide = (a,b) => {
+    return a * b;
+}
