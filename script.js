93cf1b1a83f8c5210b2ae4c062b6603e9c9d1335
WiggityWig <122560716+WiggityWig@users.noreply.github.com>
2023-03-01 07:21:41 -0500

    add operate function

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
+const zero=0;
+
 //addition
 const add = (a,b) => {
-    return a + b;
+    return total = a + b;
 }
 //subtraction
 const sub = (a,b) => {
-    return a - b;
+    return a-b;
 }
 //multiplication
 const mult = (a,b) => {
-    return a * b;
+    return total = a * b;
 }
 //division
 const divide = (a,b) => {
-    return a * b;
+    return  total = a / b;
+}
+
+ operate = (num1, oper, num2) =>{
+   if(oper == "+") {
+    return num1 + num2;
+}
+        else if(oper == "-") {
+            return num1 - num2;
+        }
+        else if (oper == "*") {
+            return num1 * num2;
+        }
+        else if (oper == "/") {
+            let result = num1 / num2;
+            if (result == "Infinity" || "NaN") {
+                return 0;
+            }
+                else {
+                    return result;
+                }
+        }
 }
